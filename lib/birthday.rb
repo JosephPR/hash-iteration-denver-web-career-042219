2f77051b1fccff77437acb82ac3b7f441d8a1673
Joseph Reilly <reilly4181@gmail.com>
2019-04-11 16:12:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,12 +3,8 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
- 
-winner
